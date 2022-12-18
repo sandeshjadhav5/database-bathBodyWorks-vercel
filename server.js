37cd56b4de036bd055978d3addb90a34fdbd8eb9
Sandesh <99778708+sandeshjadhav5@users.noreply.github.com>
2022-12-18 16:27:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults({noCors : true})
+const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
